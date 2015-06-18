5f8cffa39a36b3d4204a87ce884049e301d6875f
sjoerdk <=>
2015-06-18 23:11:40 +0200

    Added sentences by many, silly midis

---
+++ sentences/create_files.bat
@@ -0,0 +1,24 @@
+echo Flagrant system error: Whatever you did please never do it again. > sentence_1.txt
+echo 42 > sentence_2.txt
+echo seminal: adj. 1 influencing the development of future events. 2  pertaining to, containing, or consisting of semen. > sentence_3.txt
+echo Longest dutch palindrome - Legermeetsysteemregel > sentence_4.txt
+echo diag-tools send -message "Many thanks for all your help" -receiver "Michael" > sentence_5.txt
+echo I said this to my friend randomly and he was like what! So funny! > sentence_6.txt
+echo When you reinvent the wheel twice you can make a bike > sentence_7.txt
+echo Work is the curse of the drinking classes. > sentence_8.txt
+echo Oh BEHAVE! > sentence_9.txt
+echo Shagadelic baby! > sentence_10.txt
+echo There's no place like 127.0.0.1" > sentence_11.txt
+echo 99 little bugs in the code, 99 bugs in the code, patch one down, compile it around, 120 bugs in the code" > sentence_12.txt
+echo Your mama's so FAT she can't save files bigger than 4GB" > sentence_13.txt
+echo There are 10 types of people in the world: those who understand binary and those who don't. > sentence_14.txt
+echo Michael: Officially we don't support it, but ... > sentence_15.txt
+echo Michael: Probably you need to log in as Jelle with his secret password that I'm not allowed to know > sentence_16.txt
+echo Hangout 2014 - Henkjan: Maybe they first go to church? Michael: Pray to the prostate gods. > sentence_17.txt
+echo RedDwarf: Pub." Ah, yes: a meeting place where people attempt to achieve advanced states of mental incompetence by the repeated consumption of fermented vegetable drinks. - Kryten, Timeslides > sentence_18.txt
+echo RedDwarf: Who allowed this man, this pathetic man, this man who could not outwit a used teabag, to be in a position where he might endanger the entire crew? Who? Only a yogurt! - Kryten, Justice > sentence_19.txt
+echo Blackadder S4E1: "Your brain, for example, is so minute, Baldrick, that if a hungry cannibal cracked your head open, there wouldn't be enough inside to cover a small water biscuit." > sentence_20.txt
+echo Blackadder S3E4 "Well, Baldrick, I would just like to say how much I enjoyed your company and friendship but we both know that that would be an utter lie, so Sod Off and if I ever see you again, it will be a billion years too soon. > sentence_21.txt
+echo Arnold: Hasta la Vista > sentence_22.txt
+echo Contrary to popular belief, the next statement will be demonstrably false > sentence_23.txt
+echo Process each serie for the patient after the data has been send I think you miss this already. > sentence_24.txt
